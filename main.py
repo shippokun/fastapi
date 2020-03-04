384787d727f5f4ea0917216165f6c26c21f07a43
shippokun <okashi0tabetai@gmail.com>
2020-03-04 23:15:50 +0900

    feat: POSTの練習

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Query, Path
-from typing import Optional
+from fastapi import FastAPI, Query, Path, Body
+from typing import Optional, List
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -27,3 +28,45 @@ async def validation(
         path: int = Path(10)):
     return {"string": string, "integer": integer, "alias-query": alias_query,
             "path": path}
+
+
+class Data(BaseModel):
+    """request data用の型ヒントがされたクラス"""
+    string: str
+    default_name: Optional[int] = None
+    lists: List[int]
+
+@app.post('/post')
+async def declare_request_body(data: Data):
+    return {"text": "hello, {data.string}, {data.default_none}, {data.lists}"}
+
+@app.post('/post/embed')
+async def declare_request_body(data: Data = Body(..., embed=True)):
+    return {"text": "hello, {data.string}, {data.default_none}, {data.lists}"}
+
+class subDict(BaseModel):
+    strings: str
+    integer: int
+
+class NestedData(BaseModel):
+    subData: subDict
+    subDataList: List[subDict]
+
+@app.post('/post/nested')
+async def declare_nested_request_body(data: NestedData):
+    return {"text": "hello, {data.subData}, {data.subDataList}"}
+
+class ValidatatedSubData(BaseModel):
+    # 正規表現で先頭にrつけるとエスケープシーケンスは無効
+    strings: str = Field(None, min_length=2, max_length=5, regex=r'[a-b]+.')
+    integer: int = Field(..., gt=1, le=3) # required
+
+class ValidatedNestedData(BaseModel):
+    subData: ValidatatedSubData = Field(..., example={"strings": "aaa",
+        "integer":2 })
+    subDataList: List[ValidatatedSubData] = Field(...)
+
+@app.post('/validation')
+async def validation(data: ValidatedNestedData):
+    async def validation(data: ValidatedNestedData):
+        return {"text": "hello, {data.subData}, {data.subDataList}"}
